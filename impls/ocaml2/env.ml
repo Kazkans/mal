b6e932eafd4075eba051843258c4d52e04506dce
Kazkans <bajonbap@gmail.com>
2024-11-04 17:22:13 +0100

    step3 implemented

---
+++ impls/ocaml2/env.ml
@@ -0,0 +1,10 @@
+let set env s v =
+    Hashtbl.add (List.hd env) s v
+
+let rec get env s = match env with
+    | [] -> raise (Invalid_argument "Lol idiota")
+    | _ ->
+        let v = Hashtbl.find_opt (List.hd env) s in
+        (match v with
+        | Some v -> v
+        | None -> get (List.tl env) s)
---
+++ impls/ocaml2/step3_env.ml
@@ -0,0 +1,78 @@
+let repl_env = [Hashtbl.create 10]
+
+let add = function
+    | [Reader.Int a; Reader.Int b] -> Reader.Int (a+b)
+    | _ -> raise (Invalid_argument "Cannot invoke non-function")
+
+let mul = function
+    | [Reader.Int a; Reader.Int b] -> Reader.Int (a*b)
+    | _ -> raise (Invalid_argument "Cannot invoke non-function")
+
+let operation op = function
+    | [Reader.Int a; Reader.Int b] -> Reader.Int (op a b)
+    | _ -> raise (Invalid_argument "Cannot invoke non-function")
+
+let () = Env.set repl_env "+" (Reader.Fn (operation (+)))
+let () = Env.set repl_env "*" (Reader.Fn (operation ( * )))
+let () = Env.set repl_env "-" (Reader.Fn (operation (-)))
+let () = Env.set repl_env "/" (Reader.Fn (operation (/)))
+(*
+let () = Hashtbl.add repl_env "*" (Reader.Fn (operation ( * )))
+let () = Hashtbl.add repl_env "-" (Reader.Fn (operation ( - )))
+let () = Hashtbl.add repl_env "/" (Reader.Fn (operation ( / )))*)
+;;
+
+let read x = Reader.read_str x
+let rec eval env ast = match ast with
+    | Reader.Sym s -> Env.get !env s
+    | Reader.List l when List.length l <> 0 ->
+            (match (List.hd l) with
+            | Reader.Sym "def!" ->
+                let s = (match (List.nth l 1) with
+                    | Reader.Sym s -> s
+                    | _ -> raise (Invalid_argument "def! needs symbol")) in
+                let value = (eval env (List.nth l 2)) in
+                Env.set !env s value; value
+            | Reader.Sym "let*" ->
+                    (match (List.tl l) with
+                    | Reader.List bindings :: body ->
+                            let env' = ref ((Hashtbl.create 10) :: !env) in
+                            let body = (List.nth l 2) in
+                            let rec bind_pairs = (function
+                                | Reader.Sym s :: expr :: tail ->
+                                        Env.set !env s (eval env' expr);
+                                bind_pairs tail
+                                | [] -> ()
+                                | _ -> raise (Invalid_argument "bad let*")) in
+                            bind_pairs bindings;
+                            eval env' body
+                    | _ -> raise (Invalid_argument "bad_let* 2"))
+            | _ ->
+                let evaluated = List.map (eval env) l in
+                (match (List.hd evaluated) with
+                | Reader.Fn f -> f (List.tl evaluated)
+                | _ -> raise (Invalid_argument "Cannot invoke non-function")))
+    | Reader.Vector l -> Reader.Vector (List.map (eval env) l)
+    | (Reader.Map tbl) as map -> Hashtbl.filter_map_inplace (fun k v -> Some (eval env v)) tbl; map
+    | x -> x
+let print x = Printer.pr_str x
+
+let repl_env = ref repl_env
+let rep x = read x |> eval repl_env |> print
+
+let rec main () =
+        Printf.printf "user> ";
+        try 
+            let line = read_line () in
+            line |> read |> print |> print_endline;
+            line |> rep |> print_endline;
+            main ()
+        with 
+            | End_of_file ->
+                print_endline "EOF";
+                main ()
+            | Not_found ->
+                print_endline "";
+                main ()
+
+let () = main ()
